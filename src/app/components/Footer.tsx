d7fa3ff6a2be38134e692f069700c8990273f249
Kerem <kkuyucu2254@gmail.com>
2025-04-25 00:22:40 +0300

    ..

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,10 +5,10 @@ import Link from "next/link";
 const FooterComponent: React.FC = () => {
   return (
     <footer className="mt-60 bg-black rounded-tl-[64px] rounded-tr-[64px] text-white p-6 w-full transform transition-all duration-700 ease-out hover:rounded-tl-[72px] hover:rounded-tr-[72px]">
-            <div className="max-w-6xl mx-auto py-12">
+      <div className="max-w-6xl mx-auto py-12">
         <div className="flex flex-col justify-center items-center stagger-reveal">
           <div className="mb-8 text-center">
-                      {/*  <h2 className="text-4xl font-bold">İstediğin gibi miyim?</h2>
+            {/*  <h2 className="text-4xl font-bold">İstediğin gibi miyim?</h2>
            <p className="text-4xl mb-6">
               O zaman{" "}
               <Link href="https://drive.google.com/file/d/1nkFktkrbYJ1-rbCNUmAzAy1BGN5pdLFb/view?usp=sharing" target="_blank" className="hover:underline underline-offset-4">
@@ -17,8 +17,8 @@ const FooterComponent: React.FC = () => {
               göz at ve iletişime geç.
             </p>
             */}
-            <div className="flex flex-row space-x-6 space-y-0 text-4xl justify-center mx-auto stagger-reveal">
-              <Link href="mailto:keremkk.iletisim@gmail.com" className=" hover:underline underline-offset-4">
+            <div className="flex flex-wrap gap-4 text-2xl md:text-4xl justify-center mx-auto stagger-reveal">
+              <Link href="mailto:keremkk.iletisim@gmail.com" className="hover:underline underline-offset-4">
                 E-posta
               </Link>
               <Link target="_blank" href="https://github.com/keremlogg" className="hover:underline underline-offset-4">
@@ -34,13 +34,13 @@ const FooterComponent: React.FC = () => {
                 Instagram
               </Link>
             </div>
-           </div>
-           </div>
+          </div>
+        </div>
 
         <div className="mt-24 -mb-12 text-center text-sm text-gray-400">
           Bu site açık kaynaktır, yapımcısı {" "}
           <Link target="_blank" href="https://github.com/lewislosa" className="hover:underline">
-          lewislosa
+            lewislosa
           </Link>{" "}
         </div>
       </div>
