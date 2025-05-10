54da36049738020a365f2ae42b305251d818b94d
Kerem <kkuyucu2254@gmail.com>
2025-05-10 13:50:59 +0300

    sa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const productSans = localFont({
   src: [
@@ -42,6 +43,7 @@ export default function RootLayout({
   return (
     <html lang="tr">
       <body className={`${productSans.className} antialiased`}>{children}</body>
+      <SpeedInsights />
     </html>
   );
 }
