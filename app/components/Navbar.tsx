c3fd07baa9c4d34fa9b231e0521a46e5e002207e
Kerem Kuyucu <84400545+KeremKuyucu@users.noreply.github.com>
2026-02-01 20:32:34 +0300

    Remove CV link from Navbar
    Removed CV link from the navigation bar.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,7 +7,6 @@ const navLinks = [
   { href: "/", label: "Ana Sayfa", icon: FaHome },
   { href: "/about", label: "Hakkımda", icon: FaUser },
   { href: "/#projects", label: "Projects", icon: FaCode },
-  { href: "/cv", label: "CV", icon: FaFileAlt },
 ];
 
 const Navbar = () => {
