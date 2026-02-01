7580e01142817d0a344efe4f182100151f26f28d
Kerem Kuyucu <84400545+KeremKuyucu@users.noreply.github.com>
2026-02-01 20:31:11 +0300

    Update CV link in Navbar component

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,7 +7,7 @@ const navLinks = [
   { href: "/", label: "Ana Sayfa", icon: FaHome },
   { href: "/about", label: "Hakkımda", icon: FaUser },
   { href: "/#projects", label: "Projects", icon: FaCode },
-  { href: "/r/cv", label: "CV", icon: FaFileAlt },
+  { href: "/cv", label: "CV", icon: FaFileAlt },
 ];
 
 const Navbar = () => {
